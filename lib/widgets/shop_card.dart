f58c7f56c93984796d8e85fa0f02fa53b7d4fd29
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-11-22 15:21:25 +0700

    http to https

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -41,7 +41,7 @@ class ShopCard extends StatelessWidget {
           else if (item.name == "Logout") {
             final response = await request.logout(
                 // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                "http://james-zefanya-tutorial.pbp.cs.ui.ac.id/auth/logout/");
+                "https://james-zefanya-tutorial.pbp.cs.ui.ac.id/auth/logout/");
             String message = response["message"];
             if (response['status']) {
               String uname = response["username"];
